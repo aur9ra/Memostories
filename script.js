51a7e4ad1889185b5d218fefc550366f379a13cc
aur9ra <93367045+aur9ra@users.noreply.github.com>
2022-09-23 19:44:21 -0700

    a nice walk under the moonlight
    :)

--- script.js
+++ script.js
@@ -1,25 +1,50 @@
 var memo = "3141592653589793238462643383279502884197169399375105820974944592307816406286"; // What number is our memo?
+
+//These variables scale by page.
 var split_memo_nums = [""] // Split memo into pages.
 var split_memo_unds = [""] // Underscores.
 var split_memo_lens = [] // How long are all of our pages?
+var split_memo_compound_lens = [0] // How big is the sum of all numbers for all pages?
+var split_memo_user_input = [""] // Let's make sure we record our input... somewhere
+var split_memo_user_input_len = [""] //
 
 var current_page = 0 // What section are we on?
 var page_len = 0 // How long is this page?
+var page_input_len = 0 // How many characters has the user entered on this page?
 var pages = 0 // How many pages are in our memo?
 var current_num = 0 // How many digits in total have we gone through?
 
 var str_prev = ""; // What was our previous string before input?
 var current_spaces = 0; // 
-var current_length_needed = 0;
 
-var expected_length = 0;
-var running_total = 0;
+function saveUserInput(){
+    split_memo_user_input[current_page] = document.getElementById("input").value;
+}
+
+function highlightNext(){
+    saveUserInput()
+    if (split_memo_user_input[current_page].length - current_spaces == split_memo_compound_lens[current_page] & current_page != pages-1){
+        document.getElementById("nextPage").classList.remove("opacity-20")
+    } else {
+        document.getElementById("nextPage").classList.add("opacity-20")
+    }
+}
+
+function highlightPrev(){
+    if (current_page > 0){
+        document.getElementById("prevPage").classList.remove("opacity-20")
+    } else {
+        document.getElementById("prevPage").classList.add("opacity-20")
+    }
+}
 
 function main(){
+
     for (let i in memo){
         i = memo[parseInt(i)];
         split_memo_nums[current_page] = String(split_memo_nums[current_page]).concat(String(i))
         split_memo_unds[current_page] = String(split_memo_unds[current_page]).concat("_".repeat(i)+" ");
+        split_memo_compound_lens[current_page] = split_memo_compound_lens[current_page] + parseInt(i)
         page_len ++
         current_num ++
         if (i == 0){
@@ -27,41 +52,54 @@ function main(){
             split_memo_nums.push("")
             split_memo_unds.push("")
             split_memo_lens.push(page_len)
+            split_memo_compound_lens.push(0)
+            split_memo_user_input.push("")
             page_len = 0
             pages ++
-            console.log(split_memo_lens)
 
         }
     }
     current_page = 0
-    console.log(underscores)
     document.getElementById("underscores").innerHTML = split_memo_unds[current_page];
-    document.getElementById("currentNums").innerHTML = String(split_memo_nums[current_page])
+    displayNum()
     document.getElementById("currentPage").innerHTML = String(current_page+1)
+    document.getElementById("pagesCount").innerHTML = String(pages+1)
+
+    highlightNext()
+    highlightPrev()  
 }
 
 function nextPage(){
     if (current_page < pages){
+        saveUserInput()
         current_page++
         current_num += split_memo_lens[current_page]
         document.getElementById("currentPage").innerHTML = String(current_page+1)
-        document.getElementById("currentNums").innerHTML = String(split_memo_nums[current_page])
-        console.log(current_page)
-    }   
+        document.getElementById("underscores").innerHTML = split_memo_unds[current_page];
+        document.getElementById("input").value = split_memo_user_input[current_page]
+        displayNum(true)
+    }
+    highlightNext()
+    highlightPrev()
 }
 
 function prevPage(){
     if (current_page > 0){
+        saveUserInput()
         current_page--
         current_num -= split_memo_lens[current_page]
         document.getElementById("currentPage").innerHTML = String(current_page+1)
-        document.getElementById("currentNums").innerHTML = String(split_memo_nums[current_page])
-        console.log(current_page)
-    }   
+        document.getElementById("underscores").innerHTML = split_memo_unds[current_page];
+        document.getElementById("input").value = split_memo_user_input[current_page]
+        displayNum(true)
+    }
+    highlightNext()
+    highlightPrev()
 }
 
 function scrollPage(for_or_back){
-    if (for_or_back == "next"){
+    updateUserInput()
+    if (for_or_back == "next" && split_memo_user_input[current_page].length - current_spaces == split_memo_compound_lens[current_page]){
         nextPage()
     } else if (for_or_back == "prev"){
         prevPage()
@@ -69,43 +107,61 @@ function scrollPage(for_or_back){
 
 }
 
-function updateRunningTotal(){
-    running_total = 0
-    for (var i = 0; i < current_spaces; i++){
-        running_total += parseInt(memo[i])
-        
+function updateUserInput(){ // this function simply updates how many characters we've written
+    current_spaces = (document.getElementById("input").value.match(/ /g) || []).length;
+    split_memo_user_input[current_page] = document.getElementById("input").value;
+}
+
+function displayNum(initialize = false){
+    if (initialize){
+        current_spaces = (document.getElementById("input").value.match(/ /g) || []).length;
     }
-    console.log(running_total)
+
+    let current_num = split_memo_nums[current_page] // let's not manually grab this value anymore than we need...
+    let non_span_one = current_num
+    let non_span_two = ""
+
+    let to_span = current_num.charAt(current_spaces) // this variable should be one number long, and go within our span container
+    if (!(current_spaces == current_num.length-1)){
+        non_span_one = current_num.substring(0,current_spaces) // this variable should be the string leading up to the span]
+        non_span_two = current_num.substring(current_spaces+1,current_num.length) // this variable should be the string after the span.
+    }
+    // both non_span_one and non_span_two can be [0 <= n <= infinity] sized
+
+    document.getElementById("numsPreSpan").innerHTML = non_span_one
+    document.getElementById("numsSpan").innerHTML = to_span
+    document.getElementById("numsPostSpan").innerHTML = non_span_two
+
+    
 }
 
 function updateMain(){
     var str_curr = document.getElementById("input").value;
 
+    let old_spaces = current_spaces
+    //this mess of a line counts the amounts of spaces in the input.
+
+    current_spaces = (document.getElementById("input").value.match(/ /g) || []).length;
+
+    if (old_spaces != current_spaces){
+        displayNum() // if the amount of spaces changes, display the number
+    }
+
 
     // if this is true, that means that there was something erased from the input
     if ( str_curr.length < str_prev.length ){
         var erased = str_prev.charAt(str_prev.length-1)
-
-        //if we erased a space, we want to make sure our spaces are updated
-        if (erased == " "){
-            current_spaces -= 1
-        }
-        console.log(erased)
     }
 
     // if this is true, that means that there was something added to the input
     else if ( str_curr.length > str_prev.length ){
         var added = str_curr.charAt(str_curr.length-1)
 
-        //if we added a space, we want to make sure our spaces are updated
-        if (added == " "){
-            current_spaces ++
-            updateRunningTotal()
-        }
-
     
     }
 
     
     str_prev = document.getElementById("input").value;
+    highlightNext()
+    highlightPrev()
 }
